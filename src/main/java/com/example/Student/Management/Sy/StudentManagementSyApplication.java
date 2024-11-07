a5d779ad9ff4c7fa71793eed3eca89879bd04eb2
Migara Bandara <bandara-ps20045@stu.kln.ac.lk>
2024-11-07 17:01:22 +0530

    created

---
+++ src/main/java/com/example/Student/Management/Sy/StudentManagementSyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Student.Management.Sy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Student/Management/Sy/StudentManagementSyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Student.Management.Sy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
